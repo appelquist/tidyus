28485200c89953146489d3e858c3c3603bb9bded
appelquist <niklasappelquist@gmail.com>
2023-10-07 16:20:22 +0200

    Remove nextauth completely, refactor for clerk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+  const {data} = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -19,6 +17,9 @@ export default function Home() {
         <div>
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
         </div>
       </main>
     </>

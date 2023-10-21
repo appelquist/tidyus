dd3151f46e8184cd94886027de6d253d63cdc701
appelquist <niklasappelquist@gmail.com>
2023-10-21 09:06:04 +0200

    Tweak order of fetching data and add ChoresFeed

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -31,7 +31,7 @@ export const LoadingSpinner = ({ size }: Props) => {
 export const LoadingPage = () => {
   return (
     <div className="absolute right-0 top-0 flex h-screen w-screen items-center justify-center">
-      <LoadingSpinner />
+      <LoadingSpinner size={16} />
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,11 @@ import { Footer } from "~/components/footer";
 import { LoadingPage } from "~/components/loading";
 
 export default function Home() {
-  const { data, isLoading } = api.chores.getChoresWithLatestComplete.useQuery();
-  if (isLoading) return <LoadingPage />;
-  if (!data) return <div>Something went wrong</div>;
+  const { isLoaded: userLoaded } = useUser();
+  // Start fetching app asap
+  api.chores.getChoresWithLatestComplete.useQuery();
+  if (!userLoaded) return <div />;
+
   return (
     <>
       <Head>
@@ -21,14 +23,24 @@ export default function Home() {
       <main className="flex h-screen justify-center">
         <div className="h-full w-full border-x md:max-w-7xl">
           <Header />
-          <div className="grid grid-flow-row grid-cols-1 justify-items-center gap-4 p-2 md:grid-cols-3 lg:grid-cols-4">
-            {data.map((chore) => (
-              <ChoreView key={chore.id} {...chore} />
-            ))}
-          </div>
+          <ChoresFeed />
           <Footer />
         </div>
       </main>
     </>
   );
 }
+
+const ChoresFeed = () => {
+  const { data, isLoading: choresLoading } =
+    api.chores.getChoresWithLatestComplete.useQuery();
+  if (choresLoading) return <LoadingPage />;
+  if (!data) return <div>Something went wrong</div>;
+  return (
+    <div className="grid grid-flow-row grid-cols-1 justify-items-center gap-4 p-2 md:grid-cols-3 lg:grid-cols-4">
+      {data.map((chore) => (
+        <ChoreView key={chore.id} {...chore} />
+      ))}
+    </div>
+  );
+};

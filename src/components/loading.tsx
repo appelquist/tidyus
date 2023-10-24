b0604e8b7519d70d5c17dad6baa9784d14e0eb67
appelquist <niklasappelquist@gmail.com>
2023-10-24 06:45:06 +0200

    Fix LoadingPage not centering spinner

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -30,7 +30,7 @@ export const LoadingSpinner = ({ size }: Props) => {
 
 export const LoadingPage = () => {
   return (
-    <div className="absolute right-0 top-0 flex h-screen w-screen items-center justify-center">
+    <div className="absolute right-0 top-0 flex h-screen w-full items-center justify-center">
       <LoadingSpinner size={16} />
     </div>
   );
